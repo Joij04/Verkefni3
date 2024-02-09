85b3737da4d41a6c0f741fab6ccd527427f9cf35
Elma Karen <ekg22@hi.is>
2024-02-09 13:08:52 +0000

    gitignore

--- compile.cmd
+++ compile.cmd
@@ -1,1 +1,1 @@
-javac -d bin src\*.java src\is\hi\ekg22\hbv202\ass3\backend\*.java src\is\hi\ekg22\hbv202\ass3\frontend\*.java
+javac -d bin src\*.java src\is\hi\ekg22\hbv202\ass3\backend\*.java src\is\hi\ekg22\hbv202\ass3\frontend\*.java src/*.java
